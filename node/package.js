f9bda2c9120f2610d2b2f73dceea2bc695dbcb87
Aaron Gotwalt <gotwalt@everlapse.com>
2013-08-04 03:29:43 +0000

    Have some node.

---
+++ node/package.js
@@ -0,0 +1,25 @@
+{
+  "name": "everdrone",
+  "preferGlobal": true,
+  "version": "0.0.1",
+  "author": "Fran and Aaron and Barkley",
+  "description": "A cool robot for making everlapses all over your Internets.",
+  "contributors": [ 
+    {
+      "name": "Francis Lukesh",
+      "email": "fran@lukesh.com"
+    },
+    {
+      "name": "Aaron Gotwalt",
+      "email": "gotwalt@everlapse.com"
+    } 
+  ],
+  "scripts": {
+    "start": "node ./bin/everdrone",
+  },
+  "main": "./bin/everdrone",
+  "dependencies" : {
+    "gphoto2"   :  "latest"
+  }
+}
+
